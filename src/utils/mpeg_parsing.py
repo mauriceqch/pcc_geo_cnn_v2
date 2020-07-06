547e2c8eef33d1ac45ea6d59a2234babbb9b0e8a
Maurice Quach <maurice.quach@gmail.com>
2020-07-06 11:36:01 +0200

    Disable MAE code paths (requires MAE-enabled mpeg-pcc-dmetric)

--- src/utils/mpeg_parsing.py
+++ src/utils/mpeg_parsing.py
@@ -39,7 +39,7 @@ def parse_pcerror(path):
     with open(path, 'r') as f:
         s = f.read()
     try:
-        d1_mae = float(re.search(r'maeF      \(p2point\): (.+)', s, re.MULTILINE).group(1))
+        # d1_mae = float(re.search(r'maeF      \(p2point\): (.+)', s, re.MULTILINE).group(1))
         d1_mse = float(re.search(r'mseF      \(p2point\): (.+)', s, re.MULTILINE).group(1))
         d1_psnr = float(re.search(r'mseF,PSNR \(p2point\): (.+)', s, re.MULTILINE).group(1))
         d2_mse = float(re.search(r'mseF      \(p2plane\): (.+)', s, re.MULTILINE).group(1))
@@ -51,22 +51,22 @@ def parse_pcerror(path):
         y_mse = float(re.search(r'c\[0\],    F         : (.+)', s, re.MULTILINE).group(1))
         u_mse = float(re.search(r'c\[1\],    F         : (.+)', s, re.MULTILINE).group(1))
         v_mse = float(re.search(r'c\[2\],    F         : (.+)', s, re.MULTILINE).group(1))
-        y_mae = float(re.search(r'c\[0\],    maeF         : (.+)', s, re.MULTILINE).group(1))
-        u_mae = float(re.search(r'c\[1\],    maeF         : (.+)', s, re.MULTILINE).group(1))
-        v_mae = float(re.search(r'c\[2\],    maeF         : (.+)', s, re.MULTILINE).group(1))
+        # y_mae = float(re.search(r'c\[0\],    maeF         : (.+)', s, re.MULTILINE).group(1))
+        # u_mae = float(re.search(r'c\[1\],    maeF         : (.+)', s, re.MULTILINE).group(1))
+        # v_mae = float(re.search(r'c\[2\],    maeF         : (.+)', s, re.MULTILINE).group(1))
         y_psnr = float(re.search(r'c\[0\],PSNRF         : (.+)', s, re.MULTILINE).group(1))
         u_psnr = float(re.search(r'c\[1\],PSNRF         : (.+)', s, re.MULTILINE).group(1))
         v_psnr = float(re.search(r'c\[2\],PSNRF         : (.+)', s, re.MULTILINE).group(1))
     except AttributeError:
         return {
-            'd1_mae': d1_mae,
+            # 'd1_mae': d1_mae,
             'd1_mse': d1_mse,
             'd1_psnr': d1_psnr,
             'd2_mse': d2_mse,
             'd2_psnr': d2_psnr,
         }
     return {
-        'd1_mae': d1_mae,
+        # 'd1_mae': d1_mae,
         'd1_mse': d1_mse,
         'd1_psnr': d1_psnr,
         'd2_mse': d2_mse,
@@ -74,9 +74,9 @@ def parse_pcerror(path):
         'y_mse': y_mse,
         'u_mse': u_mse,
         'v_mse': v_mse,
-        'y_mae': y_mae,
-        'u_mae': u_mae,
-        'v_mae': v_mae,
+        # 'y_mae': y_mae,
+        # 'u_mae': u_mae,
+        # 'v_mae': v_mae,
         'y_psnr': y_psnr,
         'u_psnr': u_psnr,
         'v_psnr': v_psnr
